1ef8a59b5032804a5ea50e74e6d5081e61cdef60
Anton Papazov <papazov06@gmail.com>
2026-08-13 19:57:46 +0300

    Add Railway to allowed hosts

--- field_target/settings.py
+++ field_target/settings.py
@@ -29,7 +29,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '.onrender.com',
+    '.railway.app',
+]
 
 
 CSRF_TRUSTED_ORIGINS = config(
